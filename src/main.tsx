869d92cf10a4bbcd2b85d38fd557d38e26978f31
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-22 17:20:09 -0400

    feat: mount runtime approval dock

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { ApprovalDock } from "./components/ApprovalDock";
 import { RuntimeActivityDock } from "./components/RuntimeActivityDock";
 import "./styles.css";
 
@@ -9,6 +10,7 @@ const bootStartedAt = performance.now();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App bootStartedAt={bootStartedAt} />
+    <ApprovalDock />
     <RuntimeActivityDock />
   </StrictMode>,
 );
