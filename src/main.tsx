fd9204bbecc52515e64ac724332a471144cc1d72
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-22 17:17:53 -0400

    feat: mount structured runtime activity dock

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { RuntimeActivityDock } from "./components/RuntimeActivityDock";
 import "./styles.css";
 
 const bootStartedAt = performance.now();
@@ -8,5 +9,6 @@ const bootStartedAt = performance.now();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App bootStartedAt={bootStartedAt} />
+    <RuntimeActivityDock />
   </StrictMode>,
 );
