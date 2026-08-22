ef5c2bcacd4c2f549a4c131f5256955dc8116e9a
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-22 17:53:47 -0400

    feat(files): mount workspace browser dock

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { McpElicitationDock } from "./components/McpElicitationDock";
 import { McpServerDock } from "./components/McpServerDock";
 import { RuntimeActivityDock } from "./components/RuntimeActivityDock";
 import { RuntimeInputDock } from "./components/RuntimeInputDock";
+import { WorkspaceFilesDock } from "./components/WorkspaceFilesDock";
 import "./styles.css";
 
 const bootStartedAt = performance.now();
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")!).render(
     <RuntimeInputDock />
     <McpElicitationDock />
     <McpServerDock />
+    <WorkspaceFilesDock />
     <RuntimeActivityDock />
   </StrictMode>,
 );
