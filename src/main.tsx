11798fe0ccfa4aee97af95917c5a321af81b7585
Mahmud Chowdhury <ciram987@gmail.com>
2026-08-22 17:48:28 -0400

    feat(mcp): mount server manager dock

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { ApprovalDock } from "./components/ApprovalDock";
 import { McpElicitationDock } from "./components/McpElicitationDock";
+import { McpServerDock } from "./components/McpServerDock";
 import { RuntimeActivityDock } from "./components/RuntimeActivityDock";
 import { RuntimeInputDock } from "./components/RuntimeInputDock";
 import "./styles.css";
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
     <ApprovalDock />
     <RuntimeInputDock />
     <McpElicitationDock />
+    <McpServerDock />
     <RuntimeActivityDock />
   </StrictMode>,
 );
